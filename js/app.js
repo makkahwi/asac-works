6380cdc2195b301c5bf6d9ba6b8c25aaff66d35f
makkahwi <suhaibahmadai@hotmail.com>
2022-01-24 11:32:58 +0200

    Build a JS Simple Form

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-alert("Welcome")
+alert('Please bear with us to answer following questions first');
+
+var name = prompt('Please input your name');
+console.log(`Input name: ${name}`);
+
+var gender = prompt('Please input your gender, you may copy n paste of the two answers (Male | Female)');
+console.log(`Input gender: ${gender}`);
+
+var age = prompt('Please input your age');
+console.log(`Input age: ${age}`);
+
+if (age <= 0) {
+  alert(`Your input age is ${age}, which is <= 0`)
+};
+
+var welcome = prompt('Please select of you wanna see a welcoming message, Type Y for yes or N for No');
+console.log(`Input welcome: ${welcome}`);
+
+if (welcome === "Y" || welcome === "y") {
+  alert(`Welcome ${gender === "Male" ? "Mr." : gender === "Female" ? "Ms." : ""} ${name} to our system. We like to have a ${age < 40 ? "fresh blood" : `wise ${gender === "Male" ? "gentlemen" : gender === "Female" ? "ladies" : ""}`} of ${age}-year-old visitors at our website`)
+}
