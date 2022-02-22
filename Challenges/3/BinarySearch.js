451759e4df2ade4d69855f1bf9496fe05cbee8dc
makkahwi <suhaibahmadai@hotmail.com>
2022-02-22 11:23:36 +0200

    Binary Search Algo

--- Challenges/3/BinarySearch.js
+++ Challenges/3/BinarySearch.js
@@ -1,14 +1,25 @@
 const BinarySearch = (arr, key) => {
   let answer = -1;
   let middle = arr.length % 2 === 0 ? arr.length / 2 : parseInt(arr.length / 2) + 1;
-  let newArr = [...arr];
+  let start = 0;
+  let end = arr.length - 1;
 
-  while (newArr.length > 2) {
-    newArr = newArr[middle] > key ? newArr.slice(0, middle) : newArr[middle] < key ? newArr.slice(middle + 1, newArr.length) : newArr[middle] === key ? newArr = [key] : "";
-    middle = parseInt(arr.length / 2);
+  while (end - start > 2) {
+
+    if (arr[middle] === key) {
+      answer = middle;
+      break;
+    };
+
+    if (arr[middle] > key) {
+      end = parseInt(middle);
+      middle = parseInt(middle / 2) % 2 === 0 ? parseInt(middle / 2) : parseInt(middle / 2) + 1;
+    }
+    else {
+      start = parseInt(middle);
+      middle = parseInt(middle * 1.5) % 2 === 0 ? parseInt(middle * 1.5) : parseInt(middle * 1.5) + 1;
+    };
   };
 
   return answer;
 };
-
-BinarySearch([4, 8, 15, 16, 23, 42], 15)
