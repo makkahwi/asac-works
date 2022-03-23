73580afa797ad38e50259a4ba564658a27511a85
makkahwi <suhaibahmadai@hotmail.com>
2022-03-23 10:47:03 +0300

    Challenge Test

--- Challenges/5/tests/test_linked_list.py
+++ Challenges/5/tests/test_linked_list.py
@@ -2,49 +2,54 @@ import pytest
 from linked_list.linked_list import linked_list
 
 
-def test_empty_list():
-    list = linked_list()
-    expected = "List exists but has no nodes"
+def test_append(list):
+    list.append("Node B")
+    expected = "{ Node A } -> { Node B } -> None"
     actual = list.to_string()
     assert actual == expected
 
 
-def test_insert_to_list(list):
-    expected = "{ Node A } -> None"
-    actual = list.to_string()
-    assert actual == expected
-
-
-def test_header_of_empty_list():
-    list = linked_list()
-    expected = None
-    actual = list.header
-    assert actual == expected
-
-
-def test_insert_multiple(list):
+def test_append_multiple(list):
     new_list = ["Node A", "Node B"]
     expected = new_list
-    actual = list.insert_multi(new_list)
+    actual = list.append_multi(new_list)
     assert actual == expected
 
 
-def test_value1_existence_in_list(list):
-    list.insert("Node A")
-    expected = True
-    actual = list.includes("Node A")
+def test_insert_beofre_middle(list):
+    list.append("Node C")
+    list.append("Node D")
+    list.append("Node E")
+    list.append("Node F")
+    list.insert_at(int(list.length() / 2) - 1, "Node B")
+    expected = "{ Node A } -> { Node B } -> { Node C } -> { Node D } -> { Node E } -> { Node F } -> None"
+    actual = list.to_string()
     assert actual == expected
 
 
-def test_value2_existence_in_list(list):
-    list.insert("Node A")
-    expected = False
-    actual = list.includes("Node C")
+def test_insert_before_header(list):
+    list.insert_before(list.header.value, "Node B")
+    expected = "{ Node B } -> { Node A } -> None"
+    actual = list.to_string()
     assert actual == expected
 
 
-def test_return_list(list):
-    expected = "{ Node A } -> None"
+def test_insert_in_middle(list):
+    list.append("Node B")
+    list.append("Node D")
+    list.append("Node E")
+    list.insert_at(int(list.length() / 2), "Node C")
+    expected = (
+        "{ Node A } -> { Node B } -> { Node C } -> { Node D } -> { Node E } -> None"
+    )
+    actual = list.to_string()
+    assert actual == expected
+
+
+def test_insert_after_tail(list):
+    list.append("Node C")
+    list.insert_after("Node A", "Node B")
+    expected = "{ Node A } -> { Node B } -> { Node C } -> None"
     actual = list.to_string()
     assert actual == expected
 
