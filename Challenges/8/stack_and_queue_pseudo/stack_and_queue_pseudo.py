7e99da5365b9696911cbe45a136744de2ac9fb08
makkahwi <suhaibahmadai@hotmail.com>
2022-04-14 10:59:37 +0300

    Initial

--- Challenges/8/stack_and_queue_pseudo/stack_and_queue_pseudo.py
+++ Challenges/8/stack_and_queue_pseudo/stack_and_queue_pseudo.py
@@ -6,56 +6,34 @@ class Node:
 
 class Stack:
     def __init__(self):
-        self.top = None
+        self.list = []
 
     def push(self, value):
-        node = Node(value, self.top)
-        self.top = node
-
-        return self.to_string()
-
-    def push_multi(self, multi):
-        for value in multi:
-            node = Node(value, self.top)
-            self.top = node
-
-        return self.to_string()
+        node = Node(value, self.list)
+        self.list.append(node)
 
     def pop(self):
-        if not self.top:
+        if not self.list:
             raise Exception("Empty Stack")
 
-        removed = self.top.value
-        self.top = self.top.next
-
-        return removed
-
-    def pop_all(self):
-        if not self.top:
-            return self
-
-        current = self.top
-
-        while current:
-            self.top = self.top.next
-            current = self.top
+        self.list.pop()
 
     def peek(self):
-        if not self.top:
+        if not self.list:
             raise Exception("Empty Stack")
 
-        return self.top.value
+        return self.list.value
 
     def is_empty(self):
-        return not self.top
+        return not self.list
 
     def to_string(self):
         string = ""
 
-        if self.top == None:
+        if self.list == None:
             string = "Stack exists but has no nodes"
         else:
-            current = self.top
+            current = self.list
 
             while current != None:
                 string = string + "{ " + str(current.value) + " }" + " -> "
@@ -65,80 +43,23 @@ class Stack:
         return string
 
 
-class Queue:
+class PseudoQueue:
     def __init__(self):
-        self.front = None
+        self.enq = Stack()
+        self.deq = Stack()
 
     def enqueue(self, value):
-        node = Node(value)
+        self.enq.push(value)
 
-        if self.front:
-            current = self.front
-
-            while current.next:
-                current = current.next
-            else:
-                current.next = node
-        else:
-            self.front = node
-
-        return self.to_string()
-
-    def enqueue_multi(self, multi):
-        for value in multi:
-            node = Node(value)
-
-            if self.front:
-                current = self.front
-
-                while current.next:
-                    current = current.next
-                else:
-                    current.next = node
-            else:
-                self.front = node
-
-        return self.to_string()
+        return value
 
     def dequeue(self):
-        if not self.front:
+        if not self.deq.is_empty():
             raise Exception("Empty Queue")
 
-        removed = self.front.value
-        self.front = self.front.next
+        removed = self.deq.push(self.enq.pop())
 
         return removed
-
-    def dequeue_all(self):
-        if not self.front:
-            raise Exception("Empty Queue")
-
-        while self.front:
-            self.front = self.front.next
-
-    def peek(self):
-        if not self.front:
-            raise Exception("Empty Queue")
-
-        return self.front.value
-
-    def is_empty(self):
-        return not self.front
-
-    def to_string(self):
-        string = ""
-
-        if self.front == None:
-            string = "Queue exists but has no nodes"
-        else:
-            current = self.front
-
-            while current != None:
-                string = string + "{ " + str(current.value) + " }" + " -> "
-                current = current.next
-            string = string + "None"
-
-        return string
 
 
 if __name__ == "__main__":
--- Challenges/8/tests/test_stack_and_queue_pseudo.py
+++ Challenges/8/tests/test_stack_and_queue_pseudo.py
@@ -1,130 +1,23 @@
 import pytest
-from stack_and_queue.stack_and_queue import Stack, Queue
-
-# Stack Tests #############################################################################
-
-# Can successfully push onto a stack
-def test_stack_push(stack):
-    actual = stack.push("Node D")
-    expected = "{ Node D } -> { Node C } -> { Node B } -> { Node A } -> None"
-    assert actual == expected
-
-
-# Can successfully push multiple values onto a stack
-def test_stack_push_multi(stack):
-    new = ["Node D", "Node E"]
-    actual = stack.push_multi(new)
-    expected = (
-        "{ Node E } -> { Node D } -> { Node C } -> { Node B } -> { Node A } -> None"
-    )
-    assert actual == expected
-
-
-# Can successfully pop off the stack
-def test_stack_pop(stack):
-    actual = stack.pop()
-    expected = "Node C"
-    assert actual == expected
-
-
-# Can successfully empty a stack after multiple pops
-def test_stack_pop_all(stack):
-    stack.pop_all()
-    actual = stack.is_empty()
-    expected = True
-    assert actual == expected
-
-
-# Can successfully peek the next item on the stack
-def test_stack_peek(stack):
-    actual = stack.peek()
-    expected = "Node C"
-    assert actual == expected
-
-
-# Can successfully instantiate an empty stack
-def test_stack_empty():
-    stack = Stack()
-    actual = stack.is_empty()
-    expected = True
-    assert actual == expected
-
-
-# Calling pop or peek on empty stack raises exception
-def test_stack_peek_empty():
-    stack = Stack()
-    with pytest.raises(Exception):
-        stack.peek()
-
-
-@pytest.fixture
-def stack():
-    stack = Stack()
-    stack.push("Node A")
-    stack.push("Node B")
-    stack.push("Node C")
-    return stack
-
-
-# Queue Tests #############################################################################
+from stack_and_queue_pseudo.stack_and_queue_pseudo import PseudoQueue
 
 # Can successfully enqueue into a queue
-def test_queue_enqueue(queue):
+def test_enqueue(queue):
     actual = queue.enqueue("Node D")
-    expected = "{ Node A } -> { Node B } -> { Node C } -> { Node D } -> None"
-    assert actual == expected
-
-
-# Can successfully enqueue multiple values into a queue
-def test_queue_enqueue_multi(queue):
-    new = ["Node D", "Node E"]
-    actual = queue.enqueue_multi(new)
-    expected = (
-        "{ Node A } -> { Node B } -> { Node C } -> { Node D } -> { Node E } -> None"
-    )
+    expected = "Node D"
     assert actual == expected
 
 
 # Can successfully dequeue out of a queue the expected value
-def test_queue_dequeue(queue):
+def test_dequeue(queue):
     actual = queue.dequeue()
-    expected = "Node A"
+    expected = "Node C"
     assert actual == expected
-
-
-# Can successfully peek into a queue, seeing the expected value
-def test_queue_peek(queue):
-    actual = queue.peek()
-    expected = "Node A"
-    assert actual == expected
-
-
-# Can successfully empty a queue after multiple dequeues
-def test_queue_dequeue_multi(queue):
-    queue.dequeue_all()
-    actual = queue.is_empty()
-    expected = True
-    assert actual == expected
-
-
-# Can successfully instantiate an empty queue
-def test_queue_empty():
-    queue = Queue()
-    actual = queue.is_empty()
-    expected = True
-    assert actual == expected
-
-
-# Calling dequeue or peek on empty queue raises exception
-def test_queue_peek_empty():
-    queue = Queue()
-    with pytest.raises(Exception):
-        queue.peek()
 
 
 @pytest.fixture
 def queue():
-    queue = Queue()
+    queue = PseudoQueue()
     queue.enqueue("Node A")
     queue.enqueue("Node B")
     queue.enqueue("Node C")
