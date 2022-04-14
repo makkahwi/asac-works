8dd2760ca6b0da5feb5e87229954d051c876b890
makkahwi <suhaibahmadai@hotmail.com>
2022-04-14 12:26:55 +0300

    Initial Files Fix

--- Challenges/10/stack_and_queue_animal_shelter/stack_and_queue_animal_shelter.py
+++ Challenges/10/stack_and_queue_animal_shelter/stack_and_queue_animal_shelter.py
@@ -47,5 +47,81 @@ class Stack:
         return string
 
 
+class Queue:
+    def __init__(self):
+        self.front = None
+
+    def enqueue(self, value):
+        node = Node(value)
+
+        if self.front:
+            current = self.front
+
+            while current.next:
+                current = current.next
+            else:
+                current.next = node
+        else:
+            self.front = node
+
+        return self.to_string()
+
+    def enqueue_multi(self, multi):
+        for value in multi:
+            node = Node(value)
+
+            if self.front:
+                current = self.front
+
+                while current.next:
+                    current = current.next
+                else:
+                    current.next = node
+            else:
+                self.front = node
+
+        return self.to_string()
+
+    def dequeue(self):
+        if not self.front:
+            raise Exception("Empty Queue")
+
+        removed = self.front.value
+        self.front = self.front.next
+
+        return removed
+
+    def dequeue_all(self):
+        if not self.front:
+            raise Exception("Empty Queue")
+
+        while self.front:
+            self.front = self.front.next
+
+    def peek(self):
+        if not self.front:
+            raise Exception("Empty Queue")
+
+        return self.front.value
+
+    def is_empty(self):
+        return not self.front
+
+    def to_string(self):
+        string = ""
+
+        if self.front == None:
+            string = "Queue exists but has no nodes"
+        else:
+            current = self.front
+
+            while current != None:
+                string = string + "{ " + str(current.value) + " }" + " -> "
+                current = current.next
+            string = string + "None"
+
+        return string
+
+
 if __name__ == "__main__":
     pass
