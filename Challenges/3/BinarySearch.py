e08f511eb2776f003aaaf7c19815d32a23b52758
makkahwi <suhaibahmadai@hotmail.com>
2022-05-04 01:48:38 +0300

    Cover Edge Cases

--- Challenges/3/BinarySearch.py
+++ Challenges/3/BinarySearch.py
@@ -1,10 +1,23 @@
+from ast import Num
+
+
 def BinarySearch(arr, key):
+
+    if not isinstance(arr, list) or arr.length == 0:
+        return "First Input should be a non-empty array"
+
+    if not isinstance(key, Num):
+        return "Second Input should be a number"
+
     answer = -1
     middle = 0
+
     if arr.length % 2 == 0:
         middle = arr.length / 2
+
     else:
         middle = int(arr.length / 2) + 1
+
     start = 0
     end = arr.length - 1
 
@@ -16,6 +29,7 @@ def BinarySearch(arr, key):
 
         if arr[middle] > key:
             end = int(middle)
+
             if int(middle / 2) % 2 == 0:
                 middle = int(middle / 2)
             else:
@@ -23,6 +37,7 @@ def BinarySearch(arr, key):
 
         else:
             start = int(middle)
+
             if int(middle * 1.5) % 2 == 0:
                 middle = int(middle * 1.5)
             else:
