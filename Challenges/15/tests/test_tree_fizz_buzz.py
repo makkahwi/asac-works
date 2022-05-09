ef16f197be21af52a91500bf84f4562c669658fc
makkahwi <suhaibahmadai@hotmail.com>
2022-05-09 15:57:34 +0300

    Initial Test

--- Challenges/15/tests/test_tree_fizz_buzz.py
+++ Challenges/15/tests/test_tree_fizz_buzz.py
@@ -1,5 +1,19 @@
-from te import __version__
+import pytest
+from tree_fizz_buzz.tree_fizz_buzz import k_ary_tree, fizz_buzz_tree
 
 
-def test_version():
-    assert __version__ == "0.1.0"
+def test_fizz_buzz(tree):
+    actual = fizz_buzz_tree(tree)
+    expected = "{ 2 } -> { 1 } -> { Fizz } -> { Buzz } -> { 4 } -> { 6 } -> { 7 } -> { 4 } -> None"
+    assert actual == expected
+
+
+@pytest.fixture
+def tree():
+    tree = k_ary_tree()
+    tree.add_node(2, [1, 3])
+    tree.add_node(1, [5, 4])
+    tree.add_node(3, [6])
+    tree.add_node(5, [7])
+    tree.add_node(4)
+    return tree
