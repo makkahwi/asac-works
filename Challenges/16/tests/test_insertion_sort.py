6c9f8976dfff9ccd801aa9c347e20f317307c9ad
makkahwi <suhaibahmadai@hotmail.com>
2022-05-16 18:15:19 +0300

    Challenge Tests

--- Challenges/16/tests/test_insertion_sort.py
+++ Challenges/16/tests/test_insertion_sort.py
@@ -1,5 +1,43 @@
-from insertion_sort.insertion_sort import __version__
+from insertion_sort.insertion_sort import InsertionSort
+import pytest
 
 
-def test_version():
-    assert __version__ == "0.1.0"
+def test_sort():
+    arr = [8, 4, 23, 42, 16, 15]
+    actual = InsertionSort(arr)
+    expected = [4, 8, 15, 16, 23, 42]
+    assert actual == expected
+
+
+def test_sort_reverse():
+    arr = [20, 18, 12, 8, 5, -2]
+    actual = InsertionSort(arr)
+    expected = [-2, 5, 8, 12, 18, 20]
+    assert actual == expected
+
+
+def test_sort_uniques():
+    arr = [5, 12, 7, 5, 5, 7]
+    actual = InsertionSort(arr)
+    expected = [5, 5, 5, 7, 12]
+    assert actual == expected
+
+
+def test_sort_nearly():
+    arr = [2, 3, 5, 7, 13, 11]
+    actual = InsertionSort(arr)
+    expected = [2, 3, 5, 7, 11, 13]
+    assert actual == expected
+
+
+def test_sort_empty():
+    arr = []
+    actual = InsertionSort(arr)
+    expected = []
+    assert actual == expected
+
+
+def test_sort_text():
+    arr = [8, 4, 23, 42, 16, 15, "text"]
+    with pytest.raises(Exception):
+        InsertionSort(arr)
