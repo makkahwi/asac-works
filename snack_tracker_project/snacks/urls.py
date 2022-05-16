ba83bd0a912574c33b1ff3f843337800c303b378
makkahwi <suhaibahmadai@hotmail.com>
2022-05-16 11:17:39 +0300

    Details Page Link

--- snack_tracker_project/snacks/urls.py
+++ snack_tracker_project/snacks/urls.py
@@ -3,4 +3,5 @@ from .views import SnackListView, SnacksDetailView
 
 urlpatterns = [
     path("", SnackListView.as_view(), name="List"),
+    path("detail/<int:pk>", SnacksDetailView.as_view(), name="Details"),
 ]
