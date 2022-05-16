ee1558f444e954dfaac117aa0c67810ea697839d
makkahwi <suhaibahmadai@hotmail.com>
2022-05-16 18:19:03 +0300

    Blend Two Loops Into One

--- Challenges/16/insertion_sort/insertion_sort.py
+++ Challenges/16/insertion_sort/insertion_sort.py
@@ -9,13 +9,12 @@ def InsertionSort(arr):
     Sorted array
     """
 
-    for i in arr:
-        if i != int(i):
-            raise Exception("Array should have integers only")
-
     for i in range(1, len(arr)):
         j = i - 1
         temp = arr[i]
+
+        if temp != int(temp):
+            raise Exception("Array should have integers only")
 
         while j >= 0 and temp < arr[j]:
             arr[j + 1] = arr[j]
