99063801b03099a9e7abb271aa3e3916aa457bc4
makkahwi <suhaibahmadai@hotmail.com>
2022-05-21 21:02:40 +0300

    Initial Solution

--- Challenges/18/hashtable/hashtable.py
+++ Challenges/18/hashtable/hashtable.py
@@ -1,38 +1,50 @@
-def Mergesort(arr):
-    n = len(arr)
+class Hashtable:
+    def __init__(self, size=100):
+        self.size = size
+        self.table = [None] * size
 
-    if n > 1:
-        mid = n // 2
-        left = arr[:mid]
-        right = arr[mid:]
+    def set(self, key, value):
+        hkey = self.hash(key)
 
-        Mergesort(left)
-        Mergesort(right)
-        Merge(left, right, arr)
-
-        return arr
-
-
-def Merge(left, right, arr):
-    i = 0
-    j = 0
-    k = 0
-
-    while i < len(left) and j < len(right):
-        if left[i] <= right[j]:
-            arr[k] = left[i]
-            i += 1
+        if self.table[hkey] == None:
+            self.table[hkey] = [(key, value)]
         else:
-            arr[k] = right[j]
-            j += 1
+            self.table[hkey].append((key, value))
 
-        k += 1
+    def get(self, key):
+        hkey = self.hash(key)
 
-    if i == len(left):
-        for item in right[j:]:
-            arr[k] = item
-            k += 1
-    else:
-        for item in left[i:]:
-            arr[k] = item
-            k += 1
+        return self.table[hkey]
+
+    def contains(self, key):
+        hkey = self.hash(key)
+
+        if self.table[hkey] == None:
+            return False
+        else:
+            return True
+
+    def keys(self):
+        keys = []
+
+        for value in self.table:
+            if value:
+                for pair in value:
+                    keys.append(pair[0])
+
+        return keys
+
+    def hash(self, key):
+        sum = 0
+
+        for char in key:
+            ascii = ord(char)
+            sum += ascii
+
+        hkey = sum % self.size
+
+        return hkey
+
+
+if __name__ == "__main__":
+    pass
