968af8f0f2f93ac55d0989e5abe2432979a07cbe
makkahwi <suhaibahmadai@hotmail.com>
2022-05-21 21:06:44 +0300

    DocStrings

--- Challenges/18/hashtable/hashtable.py
+++ Challenges/18/hashtable/hashtable.py
@@ -4,6 +4,15 @@ class Hashtable:
         self.table = [None] * size
 
     def set(self, key, value):
+        """
+        This method hashes the key, and set the key and value pair in the table.
+
+        Input:
+        key, value
+
+        Output:
+        no output
+        """
         hkey = self.hash(key)
 
         if self.table[hkey] == None:
@@ -12,11 +21,29 @@ class Hashtable:
             self.table[hkey].append((key, value))
 
     def get(self, key):
+        """
+        This method finds the value with associated key given
+
+        Input:
+        key
+
+        Output:
+        associated value
+        """
         hkey = self.hash(key)
 
         return self.table[hkey]
 
     def contains(self, key):
+        """
+        This method indicates if the key exists in the table
+
+        Input:
+        key
+
+        Output:
+        boolean indicator
+        """
         hkey = self.hash(key)
 
         if self.table[hkey] == None:
@@ -25,6 +52,15 @@ class Hashtable:
             return True
 
     def keys(self):
+        """
+        This method to list down keys of the table
+
+        Input:
+        None
+
+        Output:
+        array of keys
+        """
         keys = []
 
         for value in self.table:
@@ -35,6 +71,15 @@ class Hashtable:
         return keys
 
     def hash(self, key):
+        """
+        This method hashes the key
+
+        Input:
+        key
+
+        Output:
+        hashed key
+        """
         sum = 0
 
         for char in key:
