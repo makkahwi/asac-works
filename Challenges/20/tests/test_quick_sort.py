4c8c67625f373053476120d64ee152aa1073cacb
makkahwi <suhaibahmadai@hotmail.com>
2022-05-28 20:58:32 +0300

    Challenge Test

--- Challenges/20/tests/test_quick_sort.py
+++ Challenges/20/tests/test_quick_sort.py
@@ -1,39 +1,26 @@
 import pytest
-from merge_sort.merge_sort import Mergesort
+from quick_sort.quick_sort import QuickSort
 
 
 def test_1():
-    acutal = Mergesort([8, 4, 23, 42, 16, 15])
+    acutal = QuickSort([8, 4, 23, 42, 16, 15], 0, 5)
     expected = [4, 8, 15, 16, 23, 42]
     assert acutal == expected
 
 
 def test_2():
-    acutal = Mergesort([20, 18, 12, 8, 5, -2])
+    acutal = QuickSort([20, 18, 12, 8, 5, -2], 0, 5)
     expected = [-2, 5, 8, 12, 18, 20]
     assert acutal == expected
 
 
 def test_3():
-    acutal = Mergesort([5, 12, 7, 5, 5, 7])
+    acutal = QuickSort([5, 12, 7, 5, 5, 7], 0, 5)
     expected = [5, 5, 5, 7, 7, 12]
     assert acutal == expected
 
 
 def test_4():
-    acutal = Mergesort([2, 3, 5, 7, 13, 11])
+    acutal = QuickSort([2, 3, 5, 7, 13, 11], 0, 5)
     expected = [2, 3, 5, 7, 11, 13]
     assert acutal == expected
-
-
-def test_sort_empty():
-    arr = []
-    actual = Mergesort(arr)
-    expected = None
-    assert actual == expected
-
-
-def test_sort_text():
-    arr = [8, 4, 23, 42, 16, 15, "text"]
-    with pytest.raises(Exception):
-        Mergesort(arr)
