127cc23ea74609fcc73a86fec62055e80d241a29
makkahwi <suhaibahmadai@hotmail.com>
2022-05-28 22:12:53 +0300

    Text Fix

--- Challenges/18/tests/test_hashtable.py
+++ Challenges/18/tests/test_hashtable.py
@@ -3,20 +3,36 @@ from hashtable.hashtable import Hashtable
 import pytest
 
 
+def test_initializing():
+    ht = Hashtable()
+    assert ht.size == 100
+    assert ht.table == [None] * 100
+
+
+def test_key_hash():
+    ht = Hashtable()
+    actual = ht.hash("key1")
+    expected = 82
+    assert actual == expected
+
+
 def test_value_set(ht):
-    actual = ht.set("key4", "value4")
-    expected = [
-        ("key1", "value1"),
-        ("key2", "value2"),
-        ("key3", "value3"),
-        ("key4", "value4"),
-    ]
+    ht.set("key4", "value4")
+    actual = ht.get("key4")
+    expected = [("key4", "value4")]
     assert actual == expected
 
 
 def test_key_based_retrieve(ht):
     actual = ht.get("key1")
-    expected = "value1"
+    expected = [("key1", "value1")]
+    assert actual == expected
+
+
+def test_key_based_retrieve_collision(ht):
+    ht.set("key3", "value33")
+    actual = ht.get("key3")
+    expected = [("key3", "value3"), ("key3", "value33")]
     assert actual == expected
 
 
@@ -28,22 +44,19 @@ def test_wrong_key_get(ht):
 
 def test_unique_keys(ht):
     actual = ht.keys()
-    expected = ["key1", "key2", "key3"]
+    expected = ["key2", "key3", "key1"]
     assert actual == expected
 
 
-def test_collision_handle():
-    pass
+def test_contains(ht):
+    actual = ht.contains("key1")
+    expected = True
+    assert actual == expected
 
 
-def test_value_with_collision():
-    pass
-
-
-def test_key_hash():
-    ht = Hashtable()
-    actual = ht.hash("key1", "value1")
-    expected = "value1"
+def test_contains_wrong(ht):
+    actual = ht.contains("key4")
+    expected = False
     assert actual == expected
 
 
