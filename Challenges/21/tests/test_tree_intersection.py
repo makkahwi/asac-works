427a4c4373612e5627bc41b84b8dbe2c5c505df4
makkahwi <suhaibahmadai@hotmail.com>
2022-05-28 21:57:49 +0300

    Initial Test

--- Challenges/21/tests/test_tree_intersection.py
+++ Challenges/21/tests/test_tree_intersection.py
@@ -1,1 +1,40 @@
 import pytest
+from tree_intersection.tree_intersection import (
+    BinaryNode,
+    BinaryTree,
+    tree_intersection,
+)
+
+
+def test_two_trees(bt1, bt2):
+    actual = tree_intersection(bt1, bt2)
+    expected = [2, 4, 6, 8]
+    assert actual == expected
+
+
+@pytest.fixture
+def bt1():
+    bt1 = BinaryTree()
+    bt1.root = BinaryNode(1)
+    bt1.root.right = BinaryNode(2)
+    bt1.root.left = BinaryNode(3)
+    bt1.root.left.right = BinaryNode(4)
+    bt1.root.left.left = BinaryNode(5)
+    bt1.root.right.right = BinaryNode(6)
+    bt1.root.right.left = BinaryNode(7)
+    bt1.root.right.right.right = BinaryNode(8)
+    return bt1
+
+
+@pytest.fixture
+def bt2():
+    bt2 = BinaryTree()
+    bt2.root = BinaryNode(12)
+    bt2.root.right = BinaryNode(2)
+    bt2.root.left = BinaryNode(11)
+    bt2.root.left.right = BinaryNode(4)
+    bt2.root.left.left = BinaryNode(10)
+    bt2.root.right.right = BinaryNode(6)
+    bt2.root.right.left = BinaryNode(9)
+    bt2.root.right.right.right = BinaryNode(8)
+    return bt2
