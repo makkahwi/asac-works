6ad62522fa28fb7e1cc4067d97d6182252a2fa17
makkahwi <suhaibahmadai@hotmail.com>
2022-05-28 20:35:39 +0300

    Pseudocode To Python

--- Challenges/20/quick_sort/quick_sort.py
+++ Challenges/20/quick_sort/quick_sort.py
@@ -1,38 +1,34 @@
-def Mergesort(arr):
-    n = len(arr)
+def QuickSort(arr, left, right):
 
-    if n > 1:
-        mid = n // 2
-        left = arr[:mid]
-        right = arr[mid:]
+    if left < right:
+        # Partition the array by setting the position of the pivot value
+        position = Partition(arr, left, right)
 
-        Mergesort(left)
-        Mergesort(right)
-        Merge(left, right, arr)
+        # Sort the left
+        QuickSort(arr, left, position - 1)
 
-        return arr
+        # Sort the right
+        QuickSort(arr, position + 1, right)
 
 
-def Merge(left, right, arr):
-    i = 0
-    j = 0
-    k = 0
+def Partition(arr, left, right):
+    # set a pivot value as a point of reference
+    pivot = arr[right]
+    # create a variable to track the largest index of numbers lower than the defined pivot
+    low = left - 1
+    for i in range(left, right):
+        if arr[i] <= pivot:
+            low += 1
+            Swap(arr, i, low)
 
-    while i < len(left) and j < len(right):
-        if left[i] <= right[j]:
-            arr[k] = left[i]
-            i += 1
-        else:
-            arr[k] = right[j]
-            j += 1
+    # place the value of the pivot location in the middle.
+    # all numbers smaller than the pivot are on the left, larger on the right.
+    Swap(arr, right, low + 1)
+    # return the pivot index point
+    return low + 1
 
-        k += 1
 
-    if i == len(left):
-        for item in right[j:]:
-            arr[k] = item
-            k += 1
-    else:
-        for item in left[i:]:
-            arr[k] = item
-            k += 1
+def Swap(arr, i, low):
+    temp = arr[i]
+    arr[i] = arr[low]
+    arr[low] = temp
