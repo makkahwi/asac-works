65853d983d6c6883e5bb42f3c5d4dfbc6d293337
makkahwi <suhaibahmadai@hotmail.com>
2022-05-29 21:33:50 +0300

    Forth Interview Fix

--- Interviews/forth/forth/forth.py
+++ Interviews/forth/forth/forth.py
@@ -1,3 +1,9 @@
+class Node:
+    def __init__(self, value=None, next=None):
+        self.value = value
+        self.next = next
+
+
 class Queue:
     def __init__(self):
         self.front = None
