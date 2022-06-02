40adab61ffe34cb9796f4e91b8de737cc7ce429c
makkahwi <suhaibahmadai@hotmail.com>
2022-06-02 13:08:03 +0300

    Function Minor Fixes

--- Challenges/03/array_binary_search/array_binary_search.py
+++ Challenges/03/array_binary_search/array_binary_search.py
@@ -1,31 +1,36 @@
 from ast import Num
 
 
-def BinarySearch(arr, key):
-
-    # Check First Input Type
-    if not isinstance(arr, list) or arr.length == 0:
-        return "First Input should be a non-empty array"
-
-    # Check Second Input Type
-    if not isinstance(key, Num):
-        return "Second Input should be a number"
-
-    answer = -1
-    middle = 0
-
-    # Define the index of middle item of arr
+def assignMiddle(length):
     # arr length is even
-    if arr.length % 2 == 0:
-        middle = arr.length / 2
+    if length % 2 == 0:
+        middle = int(length / 2)
 
     # arr length is odd
     else:
-        middle = int(arr.length / 2) + 1
+        middle = int(length / 2) + 1
+
+    return middle
+
+
+def BinarySearch(arr, key):
+
+    # Check First Input Type
+    if not isinstance(arr, list) or len(arr) == 0:
+        return "First Input should be a non-empty array"
+
+    # Check Second Input Type
+    if not isinstance(key, int):
+        return "Second Input should be a number"
+
+    answer = -1
+
+    # Define the index of middle item of arr
+    middle = assignMiddle(len(arr))
 
     # Search Range
     start = 0
-    end = arr.length - 1
+    end = len(arr) - 1
 
     while end - start > 2:
 
@@ -40,10 +45,7 @@ def BinarySearch(arr, key):
             end = int(middle)
 
             # Update middle index
-            if int(middle / 2) % 2 == 0:
-                middle = int(middle / 2)
-            else:
-                middle = int(middle / 2) + 1
+            middle = assignMiddle(int(middle / 2))
 
         # When required key is smaller than the current middle index of current search range
         else:
@@ -51,9 +53,6 @@ def BinarySearch(arr, key):
             start = int(middle)
 
             # Update middle index
-            if int(middle * 1.5) % 2 == 0:
-                middle = int(middle * 1.5)
-            else:
-                middle = int(middle * 1.5) + 1
+            middle = assignMiddle(int(middle * 1.5))
 
     return answer
