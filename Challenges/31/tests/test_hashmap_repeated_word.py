41ef9e5ccdd13d30eaa881f3ef2338b327c080af
makkahwi <suhaibahmadai@hotmail.com>
2022-06-03 07:00:22 +0300

    Challenge Test Build

--- Challenges/31/tests/test_hashmap_repeated_word.py
+++ Challenges/31/tests/test_hashmap_repeated_word.py
@@ -1,5 +1,24 @@
-from hashmap_repeated_word import __version__
+from hashmap_repeated_word.hashmap_repeated_word import repeated_word
+import pytest
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+def test_no_repeat():
+    acutal = repeated_word("hello world war three")
+    expected = "No Repeated Words"
+    assert acutal == expected
+
+
+def test_repeated_word():
+    acutal = repeated_word("hello to the world of world war three")
+    expected = "world"
+    assert acutal == expected
+
+
+def test_non_string():
+    with pytest.raises(Exception):
+        repeated_word(9)
+
+
+def test_empty_string():
+    with pytest.raises(Exception):
+        repeated_word("")
