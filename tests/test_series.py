4842dc6c90367181aca0086e1479e3660e565787
makkahwi <suhaibahmadai@hotmail.com>
2022-06-03 14:07:57 +0300

    Add More Tests

--- tests/test_series.py
+++ tests/test_series.py
@@ -1,13 +1,37 @@
 from math_series.series import fibonacci, lucas
 
 
-def test_tweleve():
-    actual = fibonacci(12)
-    expected = 89
+def test_fibonacci_zero():
+    actual = fibonacci(0)
+    expected = 0
     assert actual == expected
 
 
-def test_ten():
-    actual = lucas(10)
-    expected = 76
+def test_fibonacci_two():
+    actual = fibonacci(2)
+    expected = 1
+    assert actual == expected
+
+
+def test_fibonacci_seven():
+    actual = fibonacci(7)
+    expected = 13
+    assert actual == expected
+
+
+def test_lucas_seven():
+    actual = lucas(7)
+    expected = 29
+    assert actual == expected
+
+
+def test_lucas_twenty():
+    actual = lucas(20)
+    expected = 15127
+    assert actual == expected
+
+
+def test_lucas_five():
+    actual = lucas(5)
+    expected = 11
     assert actual == expected
