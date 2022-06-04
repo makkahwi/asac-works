91aa429d030a4288a99053d08df63108af280d81
makkahwi <suhaibahmadai@hotmail.com>
2022-06-04 19:10:23 +0300

    CSRF Fix

--- project/settings.py
+++ project/settings.py
@@ -28,6 +28,7 @@ env = environ.Env(
     DATABASE_PASSWORD=(str, ""),
     DATABASE_HOST=(str, ""),
     DATABASE_PORT=(int, 5432),
+    CSRF_TRUSTED_ORIGINS=(list, []),
 )
 
 environ.Env.read_env()
@@ -168,3 +169,5 @@ REST_FRAMEWORK = {
 
 CORS_ORIGIN_WHITELIST = tuple(env.list("ALLOWED_ORIGINS"))
 CORS_ALLOW_ALL_ORIGINS = env.bool("ALLOW_ALL_ORIGINS")
+
+CSRF_TRUSTED_ORIGINS = tuple(env.list("CSRF_TRUSTED_ORIGINS"))
