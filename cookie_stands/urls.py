4be52a94d0864a8ffd237233603f481ef4340aa7
makkahwi <suhaibahmadai@hotmail.com>
2022-06-04 19:09:28 +0300

    URL Names Update

--- cookie_stands/urls.py
+++ cookie_stands/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import CookieStandList, CookieStandDetail
 
 urlpatterns = [
-    path("", CookieStandList.as_view(), name="thing_list"),
-    path("<int:pk>/", CookieStandDetail.as_view(), name="thing_detail"),
+    path("", CookieStandList.as_view(), name="Cookie Stands list"),
+    path("<int:pk>/", CookieStandDetail.as_view(), name="Cookie Stand Details"),
 ]
