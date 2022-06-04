635fdf7f469f6cf9c2cec976444cead43f13dcc7
makkahwi <suhaibahmadai@hotmail.com>
2022-06-04 15:58:24 +0300

    Challenge ReadMe Build

--- Challenges/16/tree_max/tree_max.py
+++ Challenges/16/tree_max/tree_max.py
@@ -9,41 +9,17 @@ class BinaryTree:
     def __init__(self):
         self.root = None
 
-    # Check the traversal type
-    def type(self, type):
-        if type == "preorder":
-            return self.pre_order(self.root, [])
-        elif type == "inorder":
-            return self.in_order(self.root, [])
-        elif type == "postorder":
-            return self.post_order(self.root, [])
-
-    # Pre-order type
-    def pre_order(self, node, nodes=[]):
-        if node:
-            nodes.append(node.value)
-            nodes = self.pre_order(node.left, nodes)
-            nodes = self.pre_order(node.right, nodes)
-        return nodes
-
-    # In-order type
-    def in_order(self, node, nodes=[]):
-        if node:
-            nodes = self.in_order(node.left, nodes)
-            nodes.append(node.value)
-            nodes = self.in_order(node.right, nodes)
-        return nodes
-
-    # Post-order type
-    def post_order(self, node, nodes=[]):
-        if node:
-            nodes = self.in_order(node.left, nodes)
-            nodes = self.in_order(node.right, nodes)
-            nodes.append(node.value)
-        return nodes
-
-    # Get Maximum Value
     def tree_max(self):
+        """
+        To find the maximum node value
+
+        Input:
+        None
+
+        Output:
+        Return maximum value
+        """
+
         if self.root == None:
             raise Exception("Empty Tree")
 
@@ -67,52 +43,6 @@ class BinaryTree:
         search(self.root)
 
         return max
-
-
-class BinarySearchTree(BinaryTree):
-
-    # Add node according to BST Rule (right child is bigger & left child is smaller than parent)
-    def add_node(self, value):
-        node = BinaryNode(value)
-        root = self.root
-        current = None
-
-        if self.root == None:
-            self.root = node
-
-        while root:
-            current = root
-            if value < root.value:
-                root = root.left
-            else:
-                root = root.right
-
-        if current == None:
-            current = node
-
-        elif value < current.value:
-            current.left = node
-
-        else:
-            current.right = node
-
-    # Check value existance
-    def contains(self, value):
-        if self.root == None:
-            raise Exception("Empty Tree")
-
-        root = self.root
-
-        while root is not None:
-            if value == root.value:
-                return True
-            if root and value > root.value:
-                root = root.right
-
-            if root and value < root.value:
-                root = root.left
-
-        return False
 
 
 if __name__ == "__main__":
---
+++ notes.md
@@ -0,0 +1,16 @@
+# Late Submission Deductions List
+
+## Code Chalenges
+
+- [CC 05]
+- [CC 10]
+- [CC 15]
+- [CC 16]
+- [CC 17]
+
+## Labs
+
+- [Lab 04](https://canvas.instructure.com/courses/4333667/assignments/28344303/submissions/33677463)
+- [Lab 11](https://canvas.instructure.com/courses/4333667/assignments/28344309/submissions/33677463)
+- [Lab 13](https://canvas.instructure.com/courses/4333667/assignments/28344312/submissions/33677463)
+- [Lab 31](https://canvas.instructure.com/courses/4333667/assignments/28344325/submissions/33677463)
