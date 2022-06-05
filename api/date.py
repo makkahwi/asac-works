843594f87a14f47f67ebd645687872c4d25fd197
makkahwi <suhaibahmadai@hotmail.com>
2022-06-05 17:49:17 +0300

    Date & Hello Functions

---
+++ api/date.py
@@ -0,0 +1,18 @@
+from http.server import BaseHTTPRequestHandler
+from datetime import datetime
+
+class handler(BaseHTTPRequestHandler):
+
+  def do_GET(self):
+    #  request is successful and you will get your response
+    self.send_response(200)
+    self.send_header('Content-type', 'text/plain')
+    self.end_headers()
+    self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
+    return
+
+""""
+- if the request is correct and there are no problems: 2XX
+- if the request is not correct or there is something wrong from client side: 4XX
+- if the req/res are not correct or something wrong happened from server: 5XX
+"""
