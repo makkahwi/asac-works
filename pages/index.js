21d4cfd661389efa3a8951213163e0d5ea054f4b
makkahwi <suhaibahmadai@hotmail.com>
2022-06-08 06:35:51 +0300

    .tsx to .js Conversion

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+
+  return (
+    <>
+      <Head>
+        <title>Cookie Stand Admin - Suhaib Ahmad</title>
+      </Head>
+    </>
+
+
+  )
+}
