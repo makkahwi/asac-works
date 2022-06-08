9d0c52ccf404c72587cfe62a9c106e343d403af5
makkahwi <suhaibahmadai@hotmail.com>
2022-06-08 08:03:22 +0300

    Multi-task Form

--- components/Form/index.js
+++ components/Form/index.js
@@ -1,6 +1,6 @@
-export default function Form({ onSubmit }) {
+export default function Form({ onSubmit, action, data }) {
   const locations = [
-    { title: "Please Pick One..." },
+    { title: "Please Pick One...", value: "" },
     { title: "Amman", value: "Amman" },
     { title: "Irbid", value: "Irbid" },
     { title: "Aqaba", value: "Aqaba" },
@@ -28,9 +28,9 @@ export default function Form({ onSubmit }) {
               <div className="px-4 py-5 bg-orange-500 space-y-6 sm:p-6">
                 <div>
                   <label className="block text-sm font-medium text-gray-700">Location</label>
-                  <select name="country" className="mt-1 py-3 px-2 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-orange-500 focus:border-orange-500 sm:text-sm">
+                  <select required name="country" disabled={action === "view"} className="mt-1 py-3 px-2 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-orange-500 focus:border-orange-500 sm:text-sm">
                     {locations.map((location, i) => (
-                      <option value={location.value} key={i}>{location.title}</option>
+                      <option value={location.value} key={i} selected={location.value === data?.location}>{location.title}</option>
                     ))}
                   </select>
                 </div>
@@ -38,25 +38,32 @@ export default function Form({ onSubmit }) {
                 <div className="grid grid-cols-3 sm:col-span-12 gap-6">
                   <div>
                     <label className="block text-sm font-medium text-gray-700">Min Customers per Hour</label>
-                    <input type="number" min={0} name="minCustomers" className="mt-1 py-3 px-2 focus:ring-orange-500 focus:border-orange-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+                    <input required type="number" disabled={action === "view"} min={0} defaultValue={data?.minCustomers} name="minCustomers" className="mt-1 py-3 px-2 focus:ring-orange-500 focus:border-orange-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
                   </div>
 
                   <div>
                     <label className="block text-sm font-medium text-gray-700">Max Customers per Hour</label>
-                    <input type="number" min={0} name="maxCustomers" className="mt-1 py-3 px-2 focus:ring-orange-500 focus:border-orange-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+                    <input required type="number" disabled={action === "view"} min={0} defaultValue={data?.maxCustomers} name="maxCustomers" className="mt-1 py-3 px-2 focus:ring-orange-500 focus:border-orange-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
                   </div>
 
                   <div>
                     <label className="block text-sm font-medium text-gray-700">Average Cookies Count per Sale</label>
-                    <input type="number" min={0} step={0.01} name="avgCookies" className="mt-1 py-3 px-2 focus:ring-orange-500 focus:border-orange-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+                    <input required type="number" disabled={action === "view"} min={0} defaultValue={data?.avgCookies} step={0.01} name="avgCookies" className="mt-1 py-3 px-2 focus:ring-orange-500 focus:border-orange-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
                   </div>
                 </div>
 
-                <div className="text-right">
+                {(<div className="text-right">
                   <button type="submit" className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-orange-700 hover:bg-orange-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500">
-                    Submit
+                    {
+                      action === "create" ? "Create"
+                        : action === "duplicate" ? "Create"
+                          : action === "view" ? "Reset"
+                            : action === "update" ? "Update"
+                              : action === "delete" ? "Delete"
+                                : "Submit"
+                    }
                   </button>
-                </div>
+                </div>)}
               </div>
             </div>
           </form>
